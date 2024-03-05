8970c85832926592378c4d1dd3228f632041606a
Tiago Neves Borges <tiagosel@gmail.com>
2024-03-04 22:25:19 -0300

    refactor(controller): Standardized responses in controller by replacing magic numbers

--- src/main/java/com/rtblackboard/msvideoprocessingmanager/controller/ConfigurationController.java
+++ src/main/java/com/rtblackboard/msvideoprocessingmanager/controller/ConfigurationController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 
 @RestController
@@ -17,20 +19,24 @@ public class ConfigurationController {
     @Autowired
     private ConfigurationRepository repository;
     @PostMapping
-    public void registerConfiguration(@RequestBody @Valid ConfigurationRegistrationDTO configurationRegistrationDTO){
-        repository.save(new Configuration(configurationRegistrationDTO));
+    public ResponseEntity registerConfiguration(@RequestBody @Valid ConfigurationRegistrationDTO configurationRegistrationDTO, UriComponentsBuilder uriBuilder){
+
+        var configuration = new Configuration(configurationRegistrationDTO);
+        repository.save(configuration);
+        var uri = uriBuilder.path("/api/v1/configuration/{id}").buildAndExpand(configuration.getId()).toUri();
+        return ResponseEntity.created(uri).body(new DataConfigurationDTO(configuration));
 
     }
 
     @GetMapping
-    public Page<DataConfigurationDTO> listAll(@PageableDefault(size = 30,page = 0,sort = "inputType") Pageable page){
+    public ResponseEntity<Page<DataConfigurationDTO>> listAll(@PageableDefault(size = 30,page = 0,sort = "inputType") Pageable page){
         //Caso não estejamos usando paginação e o retorno do método for uma lista de configuracao usamos o código comentado.
         //return  repository.findAll(page).stream().map(DataConfigurationDTO::new).toList();
         //Find all busca sem filtro
         //return  repository.findAll(page).map(DataConfigurationDTO::new);
         //
-        return  repository.findAllByStatusEquals(page,"ACTIVE").map(DataConfigurationDTO::new);
-
+        var returnPage =  repository.findAllByStatusEquals(page,"ACTIVE").map(DataConfigurationDTO::new);
+        return ResponseEntity.ok(returnPage);
     }
 
 
@@ -38,24 +44,24 @@ public class ConfigurationController {
 
     @PutMapping
     @Transactional
-    public void update(@RequestBody @Valid ConfigurationUpdateDTO configurationUpdateDTO){
+    public ResponseEntity update(@RequestBody @Valid ConfigurationUpdateDTO configurationUpdateDTO){
 
         var config = repository.getReferenceById(configurationUpdateDTO.id());
         config.updateById(configurationUpdateDTO);
 
 
-
+        return ResponseEntity.ok(new DataConfigurationDTO(config));
     }
 
     @DeleteMapping("/{id}")
     @Transactional
-    public void delete(@PathVariable Long id){
+    public ResponseEntity delete(@PathVariable Long id){
 
         //repository.deleteById(id);
         var config = repository.getReferenceById(id);
         config.detete();
 
-
+        return ResponseEntity.noContent().build();
 
     }
 
